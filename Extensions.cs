bcdf785bcb770bee351c2996115c3713915d7075
Marek Grochowski <marekgrochowski@web.de>
2020-05-05 14:47:09 +0200

    Class name changed

--- Extensions.cs
+++ Extensions.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 
 namespace ConverterExtensions
 {
-    public static class ConverterExtensions
+    public static class Extensions
     {
         #region Private Fields
 
